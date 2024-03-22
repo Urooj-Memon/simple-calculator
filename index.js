6ed94d2246f2a8ad8415e536128654f0e909be64
Urooj-Memon <164159799+Urooj-Memon@users.noreply.github.com>
2024-03-23 03:53:03 +0500

    simple calculator by using inquirer
    simple calculator by using inquirer

---
+++ index.js
@@ -0,0 +1,28 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+    { message: "Enter your first number", type: "number", name: "firstnumber" },
+    { message: "Enter your second number", type: "number", name: "secondnumber" },
+    {
+        message: "select one of the operators to perform operation",
+        type: "list",
+        name: "operator",
+        choices: ["Addition", "Subtraction", "Multiplication", "Division"],
+    },
+]);
+if (answer.operator === "Addition") {
+    console.log(answer.firstnumber + answer.secondnumber);
+}
+else if (answer.operator === "Subtraction") {
+    console.log(answer.firstnumber - answer.secondnumber);
+}
+else if (answer.operator === "Multiplication") {
+    console.log(answer.firstnumber * answer.secondnumber);
+}
+else if (answer.operator === "Division") {
+    console.log(answer.firstnumber / answer.secondnumber);
+}
+else {
+    console.log("please select valid operator");
+}
+;
